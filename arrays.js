97debb6fcc19ffdc9d3c2ecca3882657a8fda63b
Shelley Brook <sbrook13@gmail.com>
2020-06-27 20:02:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,5 @@
 //first
 function addElementToBeginningOfArray(a,b){
-<<<<<<< HEAD
  a=[b,...a]
  var newArray = a
  return newArray
@@ -21,9 +20,6 @@ function addElementToEndOfArray(a,b){
 function destructivelyAddElementToEndOfArray(a,b){
   a.push(b)
   return a
-=======
-  a.unshift(b)
-  return a
 }
 //second
 function destructivelyAddElementToBeginningOfArray(a,b){
@@ -39,7 +35,6 @@ function addElementToEndOfArray(a,b){
 function destructivelyAddElementToEndOfArray(a,b){
   var newAgain = a.push(b)
   return newAgain
->>>>>>> 3122e33183668e207ae84eb31dff442ac392296c
 }
 //5
 function accessElementInArray(a,b){
@@ -54,24 +49,19 @@ function removeElementFromBeginningOfArray(a){
   var newSlice = a.slice(1)
   return newSlice
 }
-<<<<<<< HEAD
 var destructivelyRemoveElementFromEndOfArray = function(a){
   a.pop()
   return a
 }
 var removeElementFromEndOfArray = function(a){
-=======
 function destructivelyRemoveElementFromEndOfArray(a){
   a.pop()
   return a
 }
 function removeElementFromEndOfArray(a){
->>>>>>> 3122e33183668e207ae84eb31dff442ac392296c
   var newBie = a.slice(0,a.length-1)
   return newBie
 }
-
-<<<<<<< HEAD
 var chocolateBars = ['snickers','hundred grand','kitkat','skittles']
 
 
@@ -80,7 +70,5 @@ console.log(cat.slice(-1))
 
 Object.keys([1,2,3])
 
-=======
 var cat = ["Milo","Chester","Teddy","Sebastian","Chad","Frisky"]
 console.log(cat.slice(-1))
->>>>>>> 3122e33183668e207ae84eb31dff442ac392296c
